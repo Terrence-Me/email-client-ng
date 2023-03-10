6862dbe93e5115a1fe7187b7f875c8520e6a9b61
Terrence Meikle <terrence_meikle@yahoo.com>
2023-03-10 03:22:07 -0500

    add authentication state

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, tap } from 'rxjs';
 
 interface UserNameAvailableResponse {
   available: true;
@@ -20,6 +21,7 @@ interface SignupResponse {
 })
 export class AuthService {
   rootUrl = 'https://api.angular-email.com';
+  signedin$ = new BehaviorSubject(false);
 
   constructor(private http: HttpClient) {}
 
@@ -33,9 +35,12 @@ export class AuthService {
   }
 
   signup(credentials: SignupCredentials) {
-    return this.http.post<SignupResponse>(
-      this.rootUrl + '/auth/signup',
-      credentials
-    );
+    return this.http
+      .post<SignupResponse>(this.rootUrl + '/auth/signup', credentials)
+      .pipe(
+        tap(() => {
+          this.signedin$.next(true);
+        })
+      );
   }
 }
